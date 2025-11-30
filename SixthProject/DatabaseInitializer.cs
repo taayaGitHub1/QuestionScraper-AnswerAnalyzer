75f6aad738c7f7cd63da41f1fcc0cba58c346853
Taaya Rai <124600727+taayaGitHub1@users.noreply.github.com>
2025-11-30 09:27:55 +0545

    first commit

---
+++ SixthProject/DatabaseInitializer.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Data.Sqlite;
+namespace SixthProject
+{
+    public class DatabaseInitializer
+    {
+        public static void Initialize()
+        {
+            //Connect to SQLite database file
+            using var connection = new SqliteConnection("Data Source =Database/questionbank.db");
+            connection.Open();
+
+            // Execution of each command
+            foreach(var cmd in commands)
+            {
+                using var commmand = new SqliteCommand(cmd, connection);
+                command.ExecuteNonQuery();
+            }
+            //  using = X connection.Close() 
+        }
+    }
+}
