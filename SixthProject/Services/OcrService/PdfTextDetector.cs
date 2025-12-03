2761ebc270f0dcc300f0720a3fc9823bf5a7ee3c
Taaya Rai <124600727+taayaGitHub1@users.noreply.github.com>
2025-12-03 08:42:34 +0545

    PdfTextService.cs service added

--- SixthProject/Services/OcrService/PdfTextDetector.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace SixthProject.Services.OcrService
-{
-    public class Pdf
-    {
-    }
-}
---
+++ SixthProject/Services/PdfTextService.cs
@@ -0,0 +1,41 @@
+﻿using UglyToad.PdfPig;
+using UglyToad.PdfPig.Content;
+
+namespace SixthProject.Services.OcrService
+{
+    public class PdfTextDetector
+    {
+        // checks for text
+        public bool HasTextLayer(string pdfPath)
+        {
+            using var doc = PdfDocument.Open(pdfPath);
+
+            foreach (var page in doc.GetPages())
+            {
+                string text = page.Text;
+                if (!string.IsNullOrEmpty(text) && text.Length > 20)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        //extract text
+        public string ExtractText(string pdfPath)
+        {
+            using (var doc = PdfDocument.Open(pdfPath))
+            {
+                string result = "";
+
+                foreach (var page in doc.GetPages())
+                {
+                    result += page.Text + "\n";
+                }
+                return result.Trim();
+            }
+
+
+        }
+    }
+}
